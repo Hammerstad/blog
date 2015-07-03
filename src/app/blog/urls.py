1c58fa951c130888559b893f39fc8b83bcfd5e60
Eirik Mildestveit Hammerstad <emhammerstad@gmail.com>
2015-07-03 13:09:23 +0000

    Added new post form.

--- src/app/blog/urls.py
+++ src/app/blog/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('app.blog.views',
     url(r'^$', 'home', name='home'),
+    url(r'^post/new$', 'new', name='new'),
 )
--- src/app/blog/views.py
+++ src/app/blog/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
+from app.blog.forms import PostForm
+
 def home(request):
 	return render(request, 'home.html')
+	
+def new(request):
+	if request.POST:
+		post_form = PostForm(request)
+	else:
+		post_form = PostForm()
+	
+	return render(request, 'post/new.html', locals())
